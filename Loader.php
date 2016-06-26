1309a2331e9481d1c4a1b706e1b1d5934944370b
Chiori-chan <chiorigreene@gmail.com>
2016-06-25 21:15:32 -0500

    Inital framework revival

--- Loader.php
+++ Loader.php
@@ -1,32 +1,30 @@
 <?php
-	define("__ROOT__", dirname(__FILE__));
+define("__ROOT__", __DIR__);
 
-	require( "protected5/Constructor.php" );
-	
-	$chiori = new ChioriFramework5();
-	
-	// Initalize Framework Class plus Load Configuration
-	$chiori->initalizeFramework(dirname(__FILE__) . "/config.yml");
-	
-	$template = $chiori->getPluginManager()->getPluginbyName("Template");
-	
-	/* Normal Mode */
-	if ( !$template->rewriteVirtual( $_SERVER["REQUEST_URI"] ) )
-		$template->localFile( $_SERVER["REQUEST_URI"] );
-	
-	/* Compat Mode */
-	/*
-	$template = $template->rewriteVirtual( $_SERVER["REQUEST_URI"], "", true );
-	
-	if ( $template == null )
-		$template->localFile( $_SERVER["REQUEST_URI"] );
-	
-	if ( $template["compat_mode"] == "1" )
-	{
-		require( dirname(__FILE__) . "/Loader.php" );
-	}
-	else
-	{
-		$this->loadPage($template["theme"], $template["view"], $template["title"], $template["file"], $template["html"], $template["reqlevel"]);
-	}
-	*/
+require( "protected/Constructor.php" );
+
+$framework = new \Framework\Loader();
+
+// Initalize Framework Class plus Load Configuration
+$framework->initalizeFramework(dirname(__FILE__) . "/config.yml");
+
+$template = $chiori->getPluginManager()->getPluginbyName("Template");
+
+if ( !$template->rewriteVirtual( $_SERVER["REQUEST_URI"] ) )
+	$template->localFile( $_SERVER["REQUEST_URI"] );
+
+/*
+$template = $template->rewriteVirtual( $_SERVER["REQUEST_URI"], "", true );
+
+if ( $template == null )
+	$template->localFile( $_SERVER["REQUEST_URI"] );
+
+if ( $template["compat_mode"] == "1" )
+{
+	require( dirname(__FILE__) . "/Loader.php" );
+}
+else
+{
+	$this->loadPage($template["theme"], $template["view"], $template["title"], $template["file"], $template["html"], $template["reqlevel"]);
+}
+*/
